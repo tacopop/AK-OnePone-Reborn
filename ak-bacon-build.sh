11e73abed40dae921258cfd01374f1ae33bcb63b
ak <stefano.villa1976@gmail.com>
2015-02-21 06:28:15 -0700

    ak-bacon-build: compile with Linaro A15 Opt - 201502

--- ak-bacon-build.sh
+++ ak-bacon-build.sh
@@ -21,7 +21,7 @@ AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/AK-robbieL811-sabermod/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/android/AK-linaro/4.9.3-2015.02.20150221.CR83/bin/arm-cortex_a15-linux-gnueabihf-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=ak
